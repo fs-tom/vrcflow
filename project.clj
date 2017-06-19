c760bd37b69e54050b6a19027a1094fd49c44d21
joinr <vangelder@tantalus.net>
2017-06-19 17:34:28 -0500

    init stuff

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure  "1.8.0"]
+                 [spork "0.2.0.1-SNAPSHOT"]
+                 [piccolotest "0.1.2-SNAPSHOT"]])
--- src/vrcflow/core.clj
+++ src/vrcflow/core.clj
@@ -1,6 +1,61 @@
-(ns vrcflow.core)
+;;A simple flow model to examine a population of entities
+;;finding and receiving services to meet needs.
+(ns vrcflow.core
+  (:require 
+   [spork.entitysystem.store :refer :all]
+   [spork.sim.core :as sim]
+   [spork.ai  [machine :as fsm]]
+   ))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+;;Generic client processing algorithm.
+
+;;Assuming a have a client entity, with a
+;;pre-determined list of needs and an arrival
+;;time.
+
+(defentity client
+  "defines a singleton container for supply information"
+  [id & {:keys [needs arrival exit]}]
+  {:components
+   [:name    id
+    :needs   needs 
+    :arrival arrival
+    :exit    exit
+    ]})
+
+
+;;The basic model here is that needs transform into
+;;services.
+
+;;If a client has needs, it needs to try to find out
+;;how to transform them into services.
+
+;;Once a client figures out the services for its
+;;highest-priority need, it will then try to receive
+;;said services.
+
+;;Initially, clients will self-screen, that is,
+;;map their needs to services.
+
+;;Later, we'll create a screening resource, which
+;;will map needs to services via consultation.
+;;  - The client (walk-in) 
+
+;;- What is the highest priority entity?
+;;  - What is the highest priority need?
+;;    - What services are available to meet the
+;;      entity's need?
+;;    -  Does the service meet the need?
+;;        - Is there a referral? [Hidden Needs]
+;;    -  What happens when needs are met?
+;;       - Positively change metrics?
+;;    -  What happens when needs are not met?
+;;       - Client waits?
+;;         - how long?
+;;       - tries to fill next need?
+;;       - Leaves (permanently?)
+
+
+
+
