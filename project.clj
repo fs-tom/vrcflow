69ac7a824a2726aaf86814c35dbcacc42570ffef
tom <tom@that-place>
2018-02-11 22:08:24 -0600

    lein clean not working..

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :source-paths ["src" "../spork/src"]
+  :source-paths ["src" "../spork/src" ""]
   :dependencies [[org.clojure/clojure  "1.9.0"]
-                 [spork "0.2.0.5-SNAPSHOT"]
-                 [incanter "1.5.6"]
+                 [spork "0.2.0.8-SNAPSHOT"]
+                 [joinr/incanter "1.9.3-SNAPSHOT"]
                  [piccolotest "0.1.2-SNAPSHOT"]
                  [org.clojure/spec.alpha "0.1.143"]
                  [org.clojure/test.check "0.9.0"]
